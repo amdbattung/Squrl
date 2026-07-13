bfc05e33e894525fba460c01e404b287880290b3
amdbattung <175034214+amdbattung@users.noreply.github.com>
2026-07-14 00:26:21 +0800

    feat: add validators for supplier

--- src/Squrl.App/Features/Suppliers/Validators/UpdateSupplierValidator.cs
+++ src/Squrl.App/Features/Suppliers/Validators/UpdateSupplierValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Squrl.App.Extensions;
 using Squrl.App.Features.Suppliers.DTOs;
 
 namespace Squrl.App.Features.Suppliers.Validators;
@@ -7,6 +8,14 @@ public class UpdateSupplierValidator : AbstractValidator<UpdateSupplierDto>
 {
     public UpdateSupplierValidator()
     {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required.");
         
+        RuleFor(x => x.Description)
+            .BeNullOrNonWhitespace()
+            .WithMessage("{PropertyName} must not be only whitespace.")
+            .BeAscii()
+            .WithMessage("Invalid {PropertyName}.");
     }
 }
