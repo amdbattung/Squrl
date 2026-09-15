56db29a002f6512549b5acbccbb6f5b230137f33
amdbattung <175034214+amdbattung@users.noreply.github.com>
2026-09-15 15:57:51 +0800

    feat: add validators for sales orders

---
+++ src/Squrl.App/Features/SalesOrders/Validators/CreateSalesOrderValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Squrl.App.Extensions;
+using Squrl.App.Features.SalesOrders.DTOs;
+
+namespace Squrl.App.Features.SalesOrders.Validators;
+
+public class CreateSalesOrderValidator : AbstractValidator<CreateSalesOrderDto>
+{
+    public CreateSalesOrderValidator()
+    {
+        RuleFor(x => x.Customer)
+            .BeNullOrNonWhitespace()
+            .WithMessage("{PropertyName} must not only be whitespace.")
+            .BeAscii()
+            .WithMessage("Invalid {PropertyName}.");
+        
+        RuleFor(x => x.InvoiceNumber)
+            .BeNullOrNonWhitespace()
+            .WithName("Invoice Number")
+            .WithMessage("{PropertyName} must not only be whitespace.")
+            .BeAlphanumeric()
+            .WithName("Invoice Number")
+            .WithMessage("Invalid {PropertyName}.");
+        
+        //TODO: Details
+        
+        RuleFor(x => x.DateOrdered)
+            .NotNull()
+            .WithName("Date Ordered")
+            .WithMessage("{PropertyName} is required.");
+    }
+}
